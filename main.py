5c8f8ceaa0bcbd2dc8d07a6c1cfe29f02b250e83
1NO0B <noob@users.noreply.github.com>
2020-12-20 22:28:11 +0100

    created all needed methods

--- main.py
+++ main.py
@@ -11,7 +11,26 @@ board = [
         [0, 0, 0, 0, 0, 0, 0, 0, 0]
 ]
 
+#print the board in the cmd
+def printBoard(passedBoard):
+    pass
+
+
+#finds the empty fields within the board (the fields with the value: 0)
+def findEmpty():
+
+
+
+#solves the board
+def solve(passedBoard):
+    pass
+
+#checks if a value in the field is possible
+def checkPossibility(passedBoard):
+    pass
+
 
 
 
 if __name__== '__main__':
+    pass
