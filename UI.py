be19f0ff35dff69c4b2820dc5e1024b8dd3aad6d
1NO0B <noob@users.noreply.github.com>
2020-12-21 03:45:30 +0100

    added solve button

--- UI.py
+++ UI.py
@@ -32,10 +32,50 @@ class basicWindow(QWidget):
                 if time % 9 == 0:
                     row = row+1
                 layout.addWidget(button, row, column)
-                
-              
+    
+        #creates "solve" button
+        solve = QtWidgets.QPushButton("Solve!")
+        solve.setStyleSheet("background-color: darkblue;")
+        layout.addWidget(solve, 10, 8)
        
        
+
+        #sets window Title
+        self.setWindowTitle('Sudoku Solver')
+
+        self.show()
+
+class solutionWindow(QWidget):
+
+    def setupUI(self):
+
+        #creates GridLayout
+        layout = QGridLayout()
+
+        #sets window sizes
+        self.setMinimumSize(350,250)
+        self.setGeometry(0, 0, 600, 600)
+
+        #sets the layout
+        self.setLayout(layout)
+
+
+        #creates content
+        row = 0
+        column = 0
+        time = -1
+        #creates the 81 buttons
+        for i in board:
+            for j in i: 
+                time += 1  
+                #sets column to % of 3
+                column = time % 9
+                button = QtWidgets.QPushButton(str(j))
+                #checks if i is 3*n and if it is it adds 1 to row
+                if time % 9 == 0:
+                    row = row+1
+                layout.addWidget(button, row, column)
+
 
         #sets window Title
         self.setWindowTitle('Sudoku Solver')
@@ -49,15 +89,18 @@ class MainWindow(QWidget):
     def __init__(self):
 
         super(MainWindow, self).__init__()
-
+        self.solutionWindow = solutionWindow()
         self.basicWindow = basicWindow()
-
+        
         self.startBasicWindow()
+        
 
     #starts the window
     def startBasicWindow(self):
         self.basicWindow.setupUI()
 
+    def startSolutionWindow(self):
+        self.solutionWindow.setupUI()
 
 
 board = solve.getBoard()
